bff0094bdbe9f0cd26b1895d4a14e5fed4e9d80f
lusitong <wowlusitong@gmail.com>
2018-05-26 15:22:20 +0800

    Added packages: eslint, init，cli

---
+++ packages/eslint/src/index.js
@@ -0,0 +1,29 @@
+const { CLIEngine } = require('eslint');
+const ora = require('ora');
+
+const rules = require('./rules');
+
+module.exports = function () {
+  const spinner = ora('eslint').start();
+  const config = {
+    extensions: ['.js'],
+    envs: ['node'],
+    cwd: process.cwd(),
+    useEslintrc: false,
+    baseConfig: {
+      extends: 'airbnb-base'
+    },
+    rules
+  };
+
+  const engine = new CLIEngine(config);
+  const report = engine.executeOnFiles(['**/*.js']);
+  const formatter = engine.getFormatter('codeframe');
+
+  if (report.errorCount === 0) {
+    spinner.succeed('success: eslint');
+  } else {
+    spinner.stop();
+    console.log(formatter(report.results));
+  }
+};
