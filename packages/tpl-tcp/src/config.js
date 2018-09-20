0340b83c31bfd98e702b4b516e09127ee032df4c
zzswang <zzswang@gmail.com>
2018-09-20 16:05:46 +0800

    fix: dotenv for tpl-tcp

---
+++ packages/tpl-tcp/src/config.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+
+/**
+ * init dotenv
+ * priority: environment -> .env
+ *
+ * Available settings
+ *
+ * APP_PORT=9527
+ * APP_BASE_PATH=/v1
+ * APP_JWT_PUBLIC_KEY=`a public key string`
+ */
+
+dotenv.config();
+
+/**
+ *
+ * @param {*} name envrionment name
+ * @param {*} option option with { required, default }
+ * @returns {*} value
+ */
+
+export function env(name, option) {
+  const value = process.env[name];
+
+  if (option.required && !value) {
+    throw new Error(`environment ${name} is required`);
+  }
+
+  return value || option.default;
+}
+
+/**
+ * app
+ */
+
+export const PORT = env("APP_PORT", { default: 9527 });
