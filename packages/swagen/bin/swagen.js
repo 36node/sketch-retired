0a65e42a069807152b21a4c4500b1d044efb6545
邵辰 <magic--510@hotmail.com>
2018-10-09 16:26:51 +0800

    feat: swagen support local file and url remote file

--- packages/swagen/bin/swagen.js
+++ packages/swagen/bin/swagen.js
@@ -2,6 +2,7 @@
 
 var program = require("commander");
 var path = require("path");
+const is = require("../dist/util/is");
 
 var pkg = require("../package.json");
 var generators = require("../dist").default;
@@ -18,7 +19,9 @@ program
 
 const dest = program.folder || ".";
 const target = path.resolve(process.cwd(), dest);
-const swagger = path.resolve(process.cwd(), program.swagger);
+const swagger = is.URL(program.swagger)
+  ? program.swagger
+  : path.resolve(process.cwd(), program.swagger);
 const name =
   program.named ||
   path
@@ -29,12 +32,11 @@ const name =
 
 if (Object.keys(generators).includes(program.template)) {
   const generator = generators[program.template].default;
-  if (generator && typeof  generator === "function") {
+  if (generator && typeof generator === "function") {
     generators[program.template].default(target, swagger, name);
   } else {
     throw new Error(`generator ${program.template} invalid!`);
   }
-
 } else {
   throw new Error(`template ${program.template} not provided`);
 }
