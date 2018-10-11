318de59821f95ed0a7ed0b9684e15275cbb19393
zzswang <zzswang@gmail.com>
2018-10-11 10:00:06 +0800

    fix: err response with text & isomorpic fetch support

---
+++ packages/fetch/src/wrapper.js
@@ -0,0 +1,49 @@
+import qs from "qs";
+import { pickBy, identity } from "lodash";
+import createError from "http-errors";
+
+/**
+ * A wrapper of fetch
+ *
+ * @param {string} url url string
+ * @param {object} opt options
+ * @returns {Promise<object>} result {body, headers}
+ */
+export default async function(url, opt = {}) {
+  let { query, body, headers = {} } = opt;
+  let endpoint = query ? `${url}?${qs.stringify(query, { skipNulls: true })}` : url;
+
+  if (body) body = JSON.stringify(body);
+
+  headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    ...headers,
+  };
+
+  const res = await fetch(
+    endpoint,
+    pickBy(
+      {
+        ...opt,
+        body,
+        headers,
+      },
+      identity
+    )
+  );
+
+  const result = { headers: {} };
+  if (res.statusText === "No Content") {
+  } else if (!res.headers.get("content-type").includes("application/json")) {
+    result.body = await res.text();
+  } else {
+    result.body = await res.json();
+  }
+
+  // not 20x or 30x response
+  if (!res.ok) throw createError(res.status, result.body);
+
+  res.headers.forEach((val, key) => (result.headers[key] = val));
+  return result;
+}
