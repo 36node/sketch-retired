a459a0dcfcb34056e9e6a884cf702c71c16f860d
zzswang <zzswang@gmail.com>
2018-10-21 14:53:19 +0800

    fix: unsed Link

--- packages/tpl-react-redux/src/app.js
+++ packages/tpl-react-redux/src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Layout, Breadcrumb } from "antd";
 
 import Content from "./components/content";
