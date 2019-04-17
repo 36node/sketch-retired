e0276b295a895f4768ae95b73d8a0eb9b7f0cae9
Ray Chen <raychenfj@gmail.com>
2019-04-18 07:04:43 +0800

    fix: fix tpl-cra-redux, add loading (#69)
    * fix: fix tpl-cra-redux, add loading
    
    * fix: yarn lock

---
+++ packages/tpl-cra-redux/src/components/loading/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Spin, Row } from "antd";
+import styled from "styled-components";
+
+class Loading extends Component {
+  render() {
+    return (
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        className={this.props.className}
+      >
+        <Spin size="large" />
+      </Row>
+    );
+  }
+}
+
+export default styled(Loading)`
+  height: 100vh;
+  width: 100%;
+`;
