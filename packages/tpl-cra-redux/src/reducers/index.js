b0b3fed0a656ff0da456072c486756c63a7e048d
magiccaptain <magic--510@hotmail.com>
2019-06-11 17:51:16 +0800

    feat: redux-ui example (#98)

--- packages/tpl-cra-redux/src/reducers/index.js
+++ packages/tpl-cra-redux/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import { apiReducers } from "@36node/redux-api";
+import { reduxUiReducers } from "@36node/redux-ui";
 
 export default combineReducers({
   ...apiReducers,
+  ...reduxUiReducers,
 });
--- packages/tpl-cra-redux/src/selectors/index.js
+++ packages/tpl-cra-redux/src/selectors/index.js
@@ -1,6 +1,15 @@
 import * as cs from "../constants";
 import { apiSelector } from "@36node/redux-api";
+import { toggleSelector } from "@36node/redux-ui/toggle";
+import { assignSelector } from "@36node/redux-ui/assign";
+import { progressSelector } from "@36node/redux-ui/progress";
 import { createSelector } from "reselect";
+
+export const reduxUiSelectors = {
+  toggleExample: toggleSelector(cs.NS.REDUX_UI.TOGGLE_EXP),
+  assignExample: assignSelector(cs.NS.REDUX_UI.ASSIGN_EXP),
+  progressExample: progressSelector(cs.NS.REDUX_UI.PROGRESS_EXP),
+};
 
 export const petStoreSelectors = {
   listPets: apiSelector(cs.NS.PET_STORE.LIST_PETS),
