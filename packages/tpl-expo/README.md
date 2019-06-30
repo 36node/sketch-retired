9ede5a438a06990534a9ec550c95466ca481788d
SunkyWang <wangcansunking@gmail.com>
2019-06-30 11:35:54 +0800

    feat: add expo template (#94)

---
+++ packages/tpl-expo/README.md
@@ -0,0 +1,14 @@
+#Expo Template
+
+## Structure
+
+## TODO
+
+1. SDK -> Mock
+2. Redux -> Redux Saga
+3. Circleci
+4. Router -> React Navigation
+5. Test -> Jest
+6. Styled-Component
+7. 
+
---
+++ packages/tpl-expo/src/containers/Home/Home.styled.js
@@ -0,0 +1,10 @@
+import Styled from "styled-components/native";
+
+const StyledHome = Styled.View`
+    flex: 1;
+    background-color: blue;
+    align-items: center;
+    justify-content: center;
+`;
+
+export default StyledHome;
