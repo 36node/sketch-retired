edaa04f5b01c86fa9c7f5750353efe78758f14f7
zzswang <zzswang@gmail.com>
2019-08-16 16:27:16 +0800

    fix: fastman bin

--- packages/fastman/bin/fastman-ls.js
+++ packages/fastman/bin/fastman-ls.js
@@ -1,6 +1,6 @@
 const program = require("commander");
 
-const { listCollections, checkApiKey } = require("../dist");
+const { helpers, apis } = require("../dist");
 
 program.parse(process.argv);
 
@@ -8,8 +8,8 @@ const stdout = console.log.bind(console);
 const stderr = console.error.bind(console);
 
 async function ls() {
-  await checkApiKey();
-  const collections = await listCollections();
+  await helpers.checkApiKey();
+  const collections = await apis.listCollections();
   for (let c of collections) {
     stdout(c.id, c.name);
   }
