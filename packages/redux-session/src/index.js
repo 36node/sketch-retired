16096d77586c36bd989e6008fc006007fd005faf
zzswang <zzswang@36node.com>
2020-04-02 17:33:32 +0800

    chore(redux-session): export getToken function

--- packages/redux-session/src/index.js
+++ packages/redux-session/src/index.js
@@ -1,4 +1,8 @@
+import { TOKEN } from "./constants";
+
 export { makeSessionWatcher } from "./saga";
 export { withSession } from "./withSession";
 export { history } from "./history";
 export { ProtectedRoute } from "./protect";
+
+export const getToken = () => sessionStorage.getItem(TOKEN);
